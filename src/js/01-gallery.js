6c86d6ced2209fe70b8e191292f2918fb3ce0799
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2023-04-19 19:23:11 +0200

    Video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,12 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
-
 import SimpleLightbox from 'simplelightbox';
 
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+
 
 const galleryContainer = document.querySelector('.gallery');
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';  
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const time = localStorage.getItem('videoplayer-current-time');
+
+  
+player.setCurrentTime(time).then(function(seconds) {
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+            default:
+                break;
+        }
+});
