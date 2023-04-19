61d1f1ad0fb5b76dea7ed96eda3e77a2164ba943
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2023-04-19 21:34:41 +0200

    Almost finished

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,49 @@
 import throttle from "lodash.throttle";
 
-const 
+const STORAGE_KEY = "feedback-form-state";
+
+const inputText = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+refs.form.addEventListener('input', event => {
+    inputText[event.target.name] = event.target.value;
+
+    console.log(inputText);
+});
+
+populateTextarea();
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    inputText = event.currentTarget.elements;
+    
+    console.log({ email: email.value, message: message.value });
+    
+    localStorage.removeItem(STORAGE_KEY);
+    
+    event.currentTarget.reset();
+};
+
+function onTextareaInput(event) {
+    const { email, message } = event.target.value;
+
+    localStorage.setItem(STORAGE_KEY, const { email, message });
+};
+
+
+function populateTextarea(event) {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    if (savedMessage) {
+        console.log(savedMessage);
+        refs.textarea.value = savedMessage;
+    }
+};
